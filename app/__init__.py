2be312bebdade4ec37c4778a1c449fb505283714
John-5014 <kaiserjohn52@gmail.com>
2019-10-25 13:03:58 +0300

    initialize and import login manager and mail extensions

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,22 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
-from flask_sqlalchemy import SQLAlchemy   
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
+
+mail = Mail()
+
 
 def create_app(config_name):
   app = Flask(__name__)
@@ -12,11 +24,20 @@ def create_app(config_name):
   app.config.from_object(config_options[config_name])
 
   #initializing flask extensions
-  from .auth import auth as auth_blueprint
+  from .main import main as main_blueprint
+  app.register_blueprint(main_blueprint)
+  mail.init_app(app)
 
+  from .auth import auth as auth_blueprint
   app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+  # configure UploadSet
+  configure_uploads(app,photos)
+
+  
   bootstrap.init_app(app)
   db.init_app(app)
+  login_manager.init_app(app)
    
   return app
       

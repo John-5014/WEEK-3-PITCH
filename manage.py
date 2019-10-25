932d4620d7b97289780009b6889311c868ab8007
John-5014 <kaiserjohn52@gmail.com>
2019-10-25 13:12:06 +0300

    adds code for manager comand

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User,Role
+from app.models import User
 from flask_migrate import Migrate,MigrateCommand
 
 app = create_app('development')
@@ -11,7 +11,7 @@ manager.add_command('db',MigrateCommand)
   
 @manager.shell
 def make_shell_context():
-  return dict(app = app,db = db,User = User,Role = Role)
+  return dict(app = app,db = db,User = User)
 
 if __name__ == '__main__':  
   manager.run()

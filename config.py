26aeae4eb6faba7acde5151a83cfade565c6abca
John-5014 <kaiserjohn52@gmail.com>
2019-10-25 13:09:31 +0300

    include configurations for email messages

--- config.py
+++ config.py
@@ -3,8 +3,14 @@ import os
 
 class Config:
 
-
-  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:KaranjaKings@localhost/pitch'
+  SECRET_KEY = os.environ.get('SECRET_KEY')
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:KaiserKings@localhost/pitch'
+  UPLOADED_PHOTOS_DEST = 'app/static/photos'
+  MAIL_SERVER = 'smtp.googlemail.com'
+  MAIL_PORT = 587
+  MAIL_USE_TLS = True
+  MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+  MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 class ProdConfig(Config):
